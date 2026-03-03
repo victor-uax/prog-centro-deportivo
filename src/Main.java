72fcbac27911d83193615f4844ea46f43bf49671
Victor Segura <vseguranes@gmail.com>
2026-03-03 10:24:35 +0100

    fix(CentroDeportivo): corrección asignación de entrenadores y centroDeportivo

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        CentroDeportivo centroDeportivo1 = new CentroDeportivo(15);
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        Entrenador entrenador1 = new Entrenador(1, "Gulian", "hockey", 7345632);
+        Entrenador entrenador2 = new Entrenador(2, "Guanga", "fuchibol", 21);
+        Entrenador entrenador3 = new Entrenador(3, "Luca", "boxeo", 2);
+        Entrenador entrenador4 = new Entrenador(4, "El kiko", "criket", 5);
+        Entrenador entrenador5 = new Entrenador(5, "Mario", "surf", 12);
+        Entrenador entrenador6 = new Entrenador(6, "Carlos", "esgrima", 88);
+        Entrenador entrenador7 = new Entrenador(7, "Rafa", "póker", 550);
+        Entrenador entrenador8 = new Entrenador(8, "Santi", "triatlon", 1500);
+        Entrenador entrenador9 = new Entrenador(9, "PapuGod", "skate", 0);
+
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador1));
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador2));
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador3));
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador4));
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador5));
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador6));
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador7));
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador8));
+        System.out.println(centroDeportivo1.registrarEntrenador(entrenador9));
+
+        System.out.println(centroDeportivo1.mostrarEntrenadores());
+
+        System.out.println(centroDeportivo1.contarEntrenadores());
+    }
 }
