1b0a7d939dfd9665628a594500be211d2c47a113
Victor Segura <vseguranes@gmail.com>
2026-03-03 09:36:26 +0100

    feat(CentroDeportivo): método buscarEntrenador creado
    feat(CentroDeportivo): método buscarPrimerHuecoLibre creado

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -12,6 +12,11 @@ public class CentroDeportivo {
 
     }
 
+    /**
+     * Metodo para comprobar que la cantidad de entrenadores recibida sea mayor que 0
+     * @param cantidad_maxima_entrenadores
+     * @return la cantidad que ha sido recibida si es mayor que 0, o si es menor que 0 se le asigna el valor 15
+     */
     private int comprobarCantidadEntrenadores(int cantidad_maxima_entrenadores){
         int cantidadFinal;
 
@@ -23,4 +28,41 @@ public class CentroDeportivo {
         cantidadFinal = cantidad_maxima_entrenadores;
         return cantidad_maxima_entrenadores;
     }
+
+    /**
+     * Metodo para buscar un Entrenador por su id
+     * @param id
+     * @return el Entrenador entero si lo encuentra o null si no lo ha encontrado
+     */
+    public Entrenador buscarEntrenador(int id){
+        boolean seguirBuscando = true;
+        Entrenador entrenadorBuscado = null;
+
+        for( int i = 0; i < NUM_MAX_ENTRENADORES && seguirBuscando; i++){
+           if( entrenadores[i] != null && entrenadores[i].getId() == id ){
+               entrenadorBuscado = entrenadores[i];
+               seguirBuscando = false;
+           }
+        }
+        return entrenadorBuscado;
+    }
+
+    /**
+     * Metodo que sirve para buscar el primero hueco libre del array entrenadores
+     * @return la primera posición libre si la encuentra o un -1 si no ha encontrado
+     */
+    private int buscarPrimerHuecoLibre(){
+        boolean seguirBuscando = true;
+        int primerHuecoLibre = -1;
+
+        for ( int i = 0; i < NUM_MAX_ENTRENADORES && seguirBuscando; i++){
+            if( entrenadores[i] == null ){
+                primerHuecoLibre = i;
+                seguirBuscando = false;
+            }
+        }
+        return primerHuecoLibre;
+    }
+
+
 }
